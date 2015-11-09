7a82cca0a894d95c7cdb91009a4c7847ef8c5475
ambercookman <amber.cookman@gmail.com>
2015-11-08 21:02:24 -0800

    Including all current directories/files for temp landing page before modular redesign for full site use

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Mobile Nav - menu button w/ drop-down nav list
+
+jQuery(function($){
+    	     $( '.menu-btn' ).click(function(){
+    	     $('.responsive-menu').toggleClass('expand')
+    	     })
+        })
